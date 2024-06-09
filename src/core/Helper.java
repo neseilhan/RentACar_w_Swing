ac8ec965040fe502579511e9c62211a32bf6a373
Nese <neseilhannn@gmail.com>
2024-06-09 19:41:04 +0300

    isFieldEmpty,isFieldListEmpty and getLocationPoint methods created in Helper

--- src/core/Helper.java
+++ src/core/Helper.java
@@ -1,6 +1,7 @@
 package core;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Helper {
     public static void setTheme() {
@@ -15,5 +16,37 @@ public class Helper {
             }
         }
 
+    }
+    public static void showMsg (String str){
+        String msg;
+        String title;
+        switch (str) {
+            case "fill":
+                msg = "Lutfen tum alanlari doldurunuz.";
+                title = "HATA";
+                break;
+            default:
+                msg = str;
+                title = "Mesaj";
+        }
+        JOptionPane.showMessageDialog(null,str,"HATA", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public static boolean isFieldEmpty(JTextField field){
+        return field.getText().trim().isEmpty(); // if its empty return true.
+    }
+    public static boolean isFieldListEmpty(JTextField[] fieldlist){
+        for(JTextField field : fieldlist){
+            if(isFieldEmpty(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int getLocationPoint(String type, Dimension size){
+        return switch (type){
+            case "x"  -> (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2 ;
+            case "y"  -> (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2 ;
+            default -> 0;
+        };
     }
 }
--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,7 +1,11 @@
 package view;
 
+import core.Helper;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginView extends JFrame{
     private JPanel container;
@@ -21,9 +25,16 @@ public class LoginView extends JFrame{
         this.setTitle("Rent a Car");
         this.setSize(400,400); // setting size and title of the container.
 
-        int x = (Toolkit.getDefaultToolkit().getScreenSize().width - this.getSize().width) / 2 ;
-        int y = (Toolkit.getDefaultToolkit().getScreenSize().height - this.getSize().height) / 2 ;
-        this.setLocation(x,y); // container centered.
+
+        this.setLocation(Helper.getLocationPoint("x", this.getSize()),Helper.getLocationPoint("y", this.getSize())); // container centered.
         this.setVisible(true);
+
+        btn_login.addActionListener(e -> {
+            JTextField[] checkFieldList = {this.fld_username, this.fld_pass};
+            if(Helper.isFieldListEmpty(checkFieldList)){
+                Helper.showMsg("fill");
+            }
+
+        });
     }
 }
