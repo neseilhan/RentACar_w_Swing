e345d9fd0ef3ca35dfc1ce9d2a6aaaf9a4b694b9
Nese <neseilhannn@gmail.com>
2024-06-09 18:08:56 +0300

    DB class created and db connected via App class.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import core.Db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -5,14 +7,6 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
 
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/rentacar",
-                    "postgres",
-                    "postgres"
-            );
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        Connection con = Db.getInstance();
     }
 }
---
+++ src/core/Db.java
@@ -0,0 +1,39 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+    //Singleton Design Pattern
+    private static Db instance = null;
+    private Connection connection = null;
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/rentacar";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASS = "postgres";
+
+    private Db(){
+        try {
+            this.connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASS);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Connection getInstance(){
+        try {
+            if(instance == null || instance.getConnection().isClosed()){
+                instance = new Db();
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return instance.getConnection();
+    }
+
+}
